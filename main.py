1eaf171171d0dc66476f361f1c404d6ab7886653
ngocminh2309 <minhhuynhngoc99@gmail.com>
2026-06-11 13:47:46 +0700

    feat : dev b tinh gpa theo dieu kien

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # main.py gốc trên nhánh main
 def tinh_diem_gpa(diem_so):
-    # TODO: Cần cập nhật logic quy đổi sang hệ 4
-    return 0.0
+    if diem_so >= 8.5 : return 4.0
+    elif diem_so >=7.0 : return 3.0
+    else:return 2.0
+   
 
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
