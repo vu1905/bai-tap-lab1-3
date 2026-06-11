7b66c54ff86bd6bde0726667d06d575ab6ce2d76
toan0309 <46134381+toan0309@users.noreply.github.com>
2026-06-11 14:03:00 +0700

    fix: resolve confict between A and B at local

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5 : 
         return 4.0 #code cua devB
     else:
-        return round((diem_so / 10) * 4, 2) #code cua devA
+        return round((diem_so / 10) * 4, 2) #code cua deva
 
 
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
