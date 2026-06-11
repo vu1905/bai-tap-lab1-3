98af8bd11a4f3e3277f49e118b0f650a2bb81ab3
toan0309 <46134381+toan0309@users.noreply.github.com>
2026-06-11 13:47:34 +0700

    feat: dev A tinh gpa theo tuyen tinh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main.py gốc trên nhánh main
 def tinh_diem_gpa(diem_so):
-    # TODO: Cần cập nhật logic quy đổi sang hệ 4
-    return 0.0
+    # Công thức tuyến tính đơn giản của Dev A
+    return round((diem_so / 10) * 4, 2)
 
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
